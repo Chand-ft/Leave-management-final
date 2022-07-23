8d27a652e430c1dd3a6e32c052127449268c237b
Chand-ft <cftjingaete@gmail.com>
2022-07-23 20:20:13 +0200

    first

---
+++ src/app/config/declarations.ts
@@ -0,0 +1,98 @@
+import { NeutrinosAuthGuardService } from 'neutrinos-oauth-client';
+import { PageNotFoundComponent } from '../not-found.component';
+import { LayoutComponent } from '../layout/layout.component';
+import { ImgSrcDirective } from '../directives/imgSrc.directive';
+import { APP_INITIALIZER } from '@angular/core';
+import { NDataSourceService } from '../n-services/n-dataSorce.service';
+import { environment } from '../../environments/environment';
+import { NLocaleResource } from '../n-services/n-localeResources.service';
+import { NAuthGuardService } from 'neutrinos-seed-services';
+import { ArtImgSrcDirective } from '../directives/artImgSrc.directive';
+import { localesService } from '../../../baseClasses/localesService';
+
+window['neutrinos'] = {
+  environments: environment,
+};
+
+//CORE_REFERENCE_IMPORTS
+//CORE_REFERENCE_IMPORT-admin_landingComponent
+import { admin_landingComponent } from '../components/admin/admin_landing.component';
+//CORE_REFERENCE_IMPORT-staff_landingComponent
+import { staff_landingComponent } from '../components/staff/staff_landing.component';
+//CORE_REFERENCE_IMPORT-base_layoutComponent
+import { base_layoutComponent } from '../components/aa_base/base_layout.component';
+
+/**
+ * Reads datasource object and injects the datasource object into window object
+ * Injects the imported environment object into the window object
+ *
+ */
+export function startupServiceFactory(startupService: NDataSourceService) {
+  return () => {
+    return new Promise((resolve, reject) => {
+      startupService.getDataSource().then(() => {
+        localesService.init().then(() => {
+          resolve(null);
+        });
+      });
+    });
+  };
+}
+
+/**
+ *bootstrap for @NgModule
+ */
+export const appBootstrap: any = [LayoutComponent];
+
+/**
+ *declarations for @NgModule
+ */
+export const appDeclarations = [
+  ImgSrcDirective,
+  LayoutComponent,
+  PageNotFoundComponent,
+  ArtImgSrcDirective,
+  //CORE_REFERENCE_PUSH_TO_DEC_ARRAY
+  //CORE_REFERENCE_PUSH_TO_DEC_ARRAY-admin_landingComponent
+  admin_landingComponent,
+  //CORE_REFERENCE_PUSH_TO_DEC_ARRAY-staff_landingComponent
+  staff_landingComponent,
+  //CORE_REFERENCE_PUSH_TO_DEC_ARRAY-base_layoutComponent
+  base_layoutComponent,
+];
+
+/**
+ * provider for @NgModule
+ */
+export const appProviders = [
+  NDataSourceService,
+  NLocaleResource,
+  {
+    // Provider for APP_INITIALIZER
+    provide: APP_INITIALIZER,
+    useFactory: startupServiceFactory,
+    deps: [NDataSourceService],
+    multi: true,
+  },
+  NAuthGuardService,
+  //CORE_REFERENCE_PUSH_TO_PRO_ARRAY
+];
+
+/**
+ * Routes available for bApp
+ */
+
+// CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY_START
+export const appRoutes = [
+  {
+    path: 'home',
+    component: base_layoutComponent,
+    children: [
+      { path: 'admin', component: admin_landingComponent },
+      { path: 'staff', component: staff_landingComponent },
+    ],
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
+// CORE_REFERENCE_PUSH_TO_ROUTE_ARRAY_END
